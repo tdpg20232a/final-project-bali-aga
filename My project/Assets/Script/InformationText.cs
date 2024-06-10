fdd4371737f75e1c2319faaf8ae7703246329fcf
3204120804920005 <shafhi@ordent.co>
2024-06-11 06:55:28 +0700

    fixing backgorund information

--- My project/Assets/Script/InformationText.cs
+++ My project/Assets/Script/InformationText.cs
@@ -8,7 +8,8 @@ public class InformationText : MonoBehaviour
     
     public GameObject floatingTextPrefab;
     public GameObject wholeText;
-    private Vector3 adjustView = new Vector3(0, 1, 0);
+    private Vector3 adjustView = new Vector3(0, 0.85f, 0.3f);
+    private Vector3 adjustViewText = new Vector3(0, 0.85f, 0.28f);
     private Quaternion myRotation = new Quaternion();
     public Animator animator;
 
@@ -17,6 +18,7 @@ public class InformationText : MonoBehaviour
     void Start()
     {
         
+        // wholeText = GetComponent<RunningText>(); // Get the PlayAudio component
     }
 
     // Update is called once per frame
@@ -25,18 +27,26 @@ public class InformationText : MonoBehaviour
         
     }
     public void ShowInformation(Vector3 initialObjectPosition, Quaternion initialObjectRotation) {
-        wholeText = Instantiate(floatingTextPrefab, initialObjectPosition + adjustView, myRotation);
+        // wholeText = Instantiate(floatingTextPrefab, initialObjectPosition + adjustView, myRotation);
     }
     public void HideInformation() {
-        Destroy(wholeText);
+        // Destroy(wholeText);
     }
 
     public void Hide(Vector3 initialObjectPosition) {
         animator.SetTrigger("Hide");
         animator.ResetTrigger("Show");
+        if (wholeText != null) {
+            wholeText.SetActive(false);
+        }
     }
     public void Show(Vector3 initialObjectPosition) {
         floatingTextPrefab.transform.position = initialObjectPosition + adjustView;
+        if (wholeText != null) {
+            wholeText.SetActive(true);
+            floatingTextPrefab.transform.position = initialObjectPosition + adjustViewText;
+        }
+        // wholeText.transform.position = initialObjectPosition + adjustView;
         animator.SetTrigger("Show");
         animator.ResetTrigger("Hide");
     }
